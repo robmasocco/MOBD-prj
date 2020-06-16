700a59f1494ebd93e08e455843c8cbe5e66d0d6b
LHRobs <robmasocco@gmail.com>
2020-06-16 11:05:31 +0200

    Grid search for random forests.

--- Classifiers/RandomForest.py
+++ Classifiers/RandomForest.py
@@ -18,26 +18,21 @@ import numpy as np
 def random_forest_param_selection(train_x, train_y, n_folds, metric):
     # griglia degli iperparametri
     param_grid = {
-        'max_depth': [80, 90],
-        'max_features': [2, 3],
-        'min_samples_leaf': [3, 4],
-        'n_estimators': [100, 200, 300]
+        'criterion': ['gini', 'entropy'],
+        'min_samples_leaf': [1, 2, 3],
+        'n_estimators': range(120, 140, 5)
     }
+    # 2, 130, gini
 
-    clf = model_select.GridSearchCV(RandomForestClassifier(), param_grid, scoring=metric, cv=n_folds, refit=True)
+    clf = model_select.GridSearchCV(RandomForestClassifier(n_jobs=-1), param_grid, scoring=metric, cv=n_folds, refit=True, n_jobs=-1)
     clf.fit(train_x, train_y)
 
     print("Best parameters:")
-    print()
     print(clf.best_params_)
-    print()
-    print("Grid scores:")
-    print()
+    print("\nGrid scores:")
     means = clf.cv_results_['mean_test_score']
     stds = clf.cv_results_['std_test_score']
     for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print("%0.4f (+/-%0.03f) for %r"
-              % (mean, std * 2, params))
-    print()
+        print("%0.4f (+/-%0.03f) for %r\n" % (mean, std * 2, params))
 
     return clf
