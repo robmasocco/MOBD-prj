4e7f281e0ad1fd578bfe66a68bf19de4f135b6cb
LHRobs <robmasocco@gmail.com>
2020-06-18 13:02:53 +0200

    Improved code.

--- DataEvaluation.py
+++ DataEvaluation.py
@@ -12,10 +12,10 @@ import sklearn.metrics as metrics
 from DataVisualization import show_confusion_matrix
 
 
-def evaluate_classifier(classifier, test_x, test_y):
+def evaluate_classifier(classifier, data_x, data_y, matrix_title):
     """Preprocesses test set and evaluates classifiers."""
-    pred_y = classifier.predict(test_x)
-    confusion_matrix = metrics.confusion_matrix(test_y, pred_y)
-    f1_score = metrics.f1_score(test_y, pred_y, average='macro')
-    show_confusion_matrix(confusion_matrix, f1_score)
+    pred_y = classifier.predict(data_x)
+    confusion_matrix = metrics.confusion_matrix(data_y, pred_y)
+    f1_score = metrics.f1_score(data_y, pred_y, average='macro')
+    show_confusion_matrix(confusion_matrix, f1_score, matrix_title)
     return f1_score
